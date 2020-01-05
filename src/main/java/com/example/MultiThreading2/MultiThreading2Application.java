977df687205424d98167fce7eb18b0f2f9031745
Happy User Mine <furaw2@gmail.com>
2020-01-05 14:07:55 +0200

    blank project

---
+++ src/main/java/com/example/MultiThreading2/MultiThreading2Application.java
@@ -0,0 +1,14 @@
+package com.example.MultiThreading2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreading2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreading2Application.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/example/MultiThreading2/MultiThreading2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MultiThreading2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreading2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
